730c707f78ad3ad191dc57c08b80ca01309c3cba
Gazal100 <ggarg4@myseneca.ca>
2024-09-23 18:20:07 -0400

    Refactored to use response.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,9 +7,10 @@ const { version, author } = require('../../package.json');
 
 const { authenticate } = require('../auth');
 
+const { createSuccessResponse } = require('../response');
+
 // Create a router that we can use to mount our API
 const router = express.Router();
-
 
 /**
  * Expose all of our API routes on /v1/* to include an API version.
@@ -24,13 +25,13 @@ router.get('/', (req, res) => {
   // Client's shouldn't cache this response (always request it fresh)
   res.setHeader('Cache-Control', 'no-cache');
   // Send a 200 'OK' response
-  res.status(200).json({
-    status: 'ok',
-    author,
-    // Use your own GitHub URL for this!
-    githubUrl: 'https://github.com/Gazal100/fragments',
-    version,
-  });
+  res.status(200).json(
+    createSuccessResponse({
+      author,
+      githubUrl: 'https://github.com/Gazal100/fragments', // Use your own GitHub URL
+      version,
+    })
+  );
 });
 
 module.exports = router;
